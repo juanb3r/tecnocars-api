352d80077556fedbbd9db29cf9f2f8a8eb7091e7
DaggerAlmanza <85371943+DaggerAlmanza@users.noreply.github.com>
2022-02-08 20:35:19 -0500

    DG: constants.py

---
+++ process/constants.py
@@ -0,0 +1,5 @@
+CURRENTLY_LOGGED = "El usuario ya ha iniciado sesión"
+FORBIDDEN = "El usuario no puede hacer esta acción"
+LOGGED = "Sesión iniciada"
+LOGGED_OUT = "Sesión cerrada"
+NOT_LOGGED = "Usuario no ha iniciado sesión"
